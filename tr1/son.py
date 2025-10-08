9f59bfd2bd65f5898314b65f3d60fb15e2d3e7a2
oussamabenalisst <benalio248@gmail.com>
2025-10-08 20:33:43 +0100

    init

---
+++ tr1/son.py
@@ -0,0 +1,22 @@
+import wave
+import math
+import struct
+
+nom_fichier = "exemple.wav"
+framerate = 44100
+nchannels = 1
+sampwith = 2
+nframes = 44100
+comptype = "NONE"
+compname = "not compressed"
+with wave.open(nom_fichier, "w") as fichier_audio:
+    fichier_audio.setparams(
+        (nchannels, sampwith, framerate, nframes, comptype, compname)
+    )
+    valeur = (
+        int(32767.0 * math.sin(2.0 * math.pi * 440.0 * i / float(framerate)))
+        for i in range(nframes)
+    )
+    for i in range(nframes):
+        valeur = int(32767.0 * math.sin(2.0 * math.pi * 440.0 * i / float(framerate)))
+        fichier_audio.writeframes(struct.pack("<h", valeur))
